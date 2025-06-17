191e5787e9764867c867d7ebda61af30d00d8c70
LilPak0 <gomes_andre66@hotmail.com>
2025-06-17 20:09:23 +0100

    new Page

--- spotidados/src/app/AccountStats/page.tsx
+++ spotidados/src/app/AccountStats/page.tsx
@@ -1,21 +1,39 @@
 
-
 export default function Home() {
   return (
     <div className="bg-[url(/main_background.png)] bg-cover bg-center h-dvh text-black">
-      <div className="bg-[url(/background_before.png)] bg-center bg-contain bg-no-repeat h-[85%]">
-        <div className="">
-            <div className="bg-[url(/profile_frame.png)] relative top-[200px] w-[70px] h-[70px]">
-                <img className="" width={"50px"} src="/profile_picture.png" alt="" />
-            </div>
-            <div className="pt-[200px]">
-                <p className="text-[20px] font-bold text-center mb-[20px]">Top 100 Songs</p>
-            </div>
+      <div className="bg-[url(/background_before.png)] bg-center bg-contain bg-no-repeat h-[85%] overflow-hidden">
+        <div className=" flex flex-col items-center mt-[5rem]">
+          <div className="bg-[url(/profile_frame.png)] bg-contain w-[180px] h-[180px] flex items-center justify-center">
+            <img className="object-contain w-[118px]" src="/profile_picture.png" alt="Profile" />
+          </div>
+          <p className="text-[32px] mt-4">Hi, Callax13</p>
+        </div>
+
+        <div className="mt-[5rem] flex flex-col justify-center items-center px-8 w-full max-w-md">
+           <div className="space-y-6 text-center">
+             <div>
+               <p className="text-[1.12rem]">You've played <strong>1394</strong> songs</p>
+               <p className="text-[1.12rem]">A total of <strong>159</strong> different tracks</p>
+             </div>
+
+             <div>
+               <p className="text-[1.12rem]">You've heard <strong>14285</strong> minutes</p>
+               <p className="text-[1.12rem]">An average of <strong>235</strong> minutes per day</p>
+              </div>
+
+             <div>
+               <p className="text-[1.12rem]">You prefer to listen to music at night</p>
+               <p className="text-[1.12rem]">Your <strong>Summer</strong> is always full of music</p>
+             </div>
+           </div>
         </div>
       </div>
+
       <div className="bg-[url(/nav_bar.png)] bg-no-repeat bg-center bg-contain">
           <img className="relative top-[-20px]" src="/icon_nav.png" alt="Nav bar icon" />
       </div>
     </div>
   );
 }
+
---
+++ spotidados/src/app/Settings/page.tsx
@@ -0,0 +1,31 @@
+export default function Home() {
+  return (
+    <div className="bg-[url(/main_background.png)] bg-cover bg-center h-dvh text-black overflow-hidden">
+      <div className="bg-[url(/background_before.png)] bg-center bg-contain bg-no-repeat h-[85%] overflow-hidden">
+        <div className="ml-[3rem] flex flex-row items-center mt-[4rem]">
+          <div className="bg-[url(/profile_frame.png)] bg-contain w-[140px] h-[140px] flex items-center justify-center mr-[1rem]">
+            <img className="object-contain w-[92px]" src="/profile_picture.png" alt="Profile" />
+          </div>
+          <div className="text-center">
+            <p className="text-[2rem] m-0">callax13</p>
+            <button className="rounded-[10px] bg-[#D9D9D9] w-[10rem] h-[0.8rem] inset-shadow-sm inset-shadow-gray-400 m-0"></button>
+            <p className="text-[1rem] m-0">User since 2006</p>
+          </div>
+        </div>
+
+        <div className="ml-[4rem] mt-[2rem] text-[1.15rem]">
+            <p className="mb-2">Email: ped********@gmail.com</p>
+            <p className="mb-2">Membership Plan: None</p>
+            <p className="mb-2">Status: Missed last payment</p>
+        </div>
+
+      </div>
+
+
+
+      <div className="bg-[url(/nav_bar.png)] bg-no-repeat bg-center bg-contain">
+          <img className="relative top-[-20px]" src="/icon_nav.png" alt="Nav bar icon" />
+      </div>
+    </div>
+  );
+}
